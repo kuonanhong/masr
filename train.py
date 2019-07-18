5e4da5ea91aece567ca4b0b95b8451d14a7c0fa1
Yihui Xiong <yihui.xiong@hotmail.com>
2019-07-18 17:52:05 +0800

    decrease batch size

--- train.py
+++ train.py
@@ -12,8 +12,8 @@ import json
 
 def train(
     model,
-    epochs=1000,
-    batch_size=64,
+    epochs=100,
+    batch_size=32,
     train_index_path="train.index",
     dev_index_path="dev.index",
     labels_path="labels.json",
